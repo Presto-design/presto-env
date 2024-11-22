e9adc601483b1f2fc530ea3f1dc6481e38c4b242
David Mack <davidhughhenrymack@gmail.com>
2024-11-21 16:33:46 -0800

    moar

--- presto_env/env.py
+++ presto_env/env.py
@@ -15,7 +15,7 @@ class PrestoEnv:
     PRESTO_DATASETS = "PRESTO_DATASETS"
 
     @staticmethod
-    def get_temp_folder() -> Path:
+    def create_temp_folder() -> Path:
         if PrestoEnv.PRESTO_TEMPORARY in os.environ:
             folder_name = f"{datetime.now().strftime('%Y%m%d-%H-%M')}_{uuid.uuid4()}"
             path = Path(os.environ[PrestoEnv.PRESTO_TEMPORARY]) / folder_name
@@ -26,7 +26,7 @@ class PrestoEnv:
             return path
 
     @staticmethod
-    def get_run_folder(project: str, run_id: str) -> Path:
+    def run_folder(project: str, run_id: str) -> Path:
         if IS_MODAL_REMOTE():
             # You're running in the run folder!
             return Path("./")
@@ -37,7 +37,7 @@ class PrestoEnv:
                 return Path("./runs") / project / run_id
 
     @staticmethod
-    def get_bulk_assets_folder() -> Path:
+    def bulk_assets_folder() -> Path:
         if not PrestoEnv.PRESTO_BULK_ASSETS in os.environ:
             raise EnvironmentError(
                 f"Please set ${PrestoEnv.PRESTO_BULK_ASSETS} environment variable"
@@ -46,7 +46,7 @@ class PrestoEnv:
         return Path(os.environ[PrestoEnv.PRESTO_BULK_ASSETS])
 
     @staticmethod
-    def get_datasets_folder() -> Path:
+    def datasets_folder() -> Path:
         if PrestoEnv.PRESTO_DATASETS in os.environ:
             return Path(os.environ[PrestoEnv.PRESTO_DATASETS])
         else:
--- presto_env/tests.py
+++ presto_env/tests.py
@@ -30,5 +30,25 @@ class TestPrestoEnv(unittest.TestCase):
         self.assertEqual(datasets_folder, Path("/datasets"))
 
 
+class TestRun(unittest.TestCase):
+
+    def test_generate_run_id(self):
+        run_id = generate_run_id()
+        self.assertIsInstance(run_id, str)
+        self.assertIn("-", run_id)
+
+
+class TestModal(unittest.TestCase):
+
+    @patch("torch.cuda.is_available", return_value=True)
+    @patch("torch.cuda.get_device_name", return_value="NVIDIA A100-SXM4-80GB")
+    @patch("torch.cuda.device_count", return_value=2)
+    def test_get_modal_gpu_price_per_hour(
+        self, mock_is_available, mock_get_device_name, mock_device_count
+    ):
+        price = get_modal_gpu_price_per_hour()
+        self.assertEqual(price, MODAL_GPU_PER_HOUR_PRICE["NVIDIA A100-SXM4-80GB"] * 2)
+
+
 if __name__ == "__main__":
     unittest.main()
